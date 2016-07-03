61b8e0e13dc1de448f22bea106c4a807f6a4f09f
Zerol <zerolfx0@gmail.com>
2016-07-04 06:22:06 +0800

    Project Start

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic.list import ListView
+from blog.models import Article, Category
+import markdown2
+
+
+class IndexView(ListView):
+    template_name = "index.html"
+    context_object_name = "article_list"
+
+    def get_queryset(self):
+        article_list = Article.objects.filter(status='p')
+        for article in article_list:
+            article.body = markdown2.markdown(article.body, )
+        return article_list
+
+    def get_context_data(self, **kwargs):
+        kwargs['category_list'] = Category.objects.all().order_by('name')
+        return super(IndexView, self).get_context_data(**kwargs)
+
+
+
