1a0b396287e2ed240777d1a29015aeb267f8e881
Zerol <zerolfx0@gmail.com>
2016-07-05 14:50:49 +0800

    detail 1

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from blog.models import Article, Category
 import markdown2
 
@@ -14,12 +15,25 @@ class IndexView(ListView):
     def get_queryset(self):
         article_list = Article.objects.filter(status='p')
         for article in article_list:
-            article.body = markdown2.markdown(article.body, )
+            article.body = markdown2.markdown(article.body)
         return article_list
 
     def get_context_data(self, **kwargs):
         kwargs['category_list'] = Category.objects.all().order_by('name')
         return super(IndexView, self).get_context_data(**kwargs)
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "detail.html"
+    context_object_name = "article"
+    pk_url_kwarg = 'article_id'
+
+    def get_object(self):
+        obj = super(ArticleDetailView, self).get_object()
+        obj.body = markdown2.markdown(obj.body)
+        return obj
+
 
 
 
