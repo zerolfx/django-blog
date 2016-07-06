e2918f47dfb6c0d95d3f2d7ad957eb8e026d7440
Zerol <zerolfx0@gmail.com>
2016-07-06 11:45:45 +0800

    fenye (ok)

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from blog import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<page>\d+)$', views.IndexView.as_view()),
+    url(r'^(?P<page>\d+)$', views.IndexView.as_view(), name='pages'),
     url(r'^article/(?P<article_id>\d+)$', views.ArticleDetailView.as_view(), name='detail'),
     # url(r'^blog/category/(?P<cate_id>\d+)$', views.)
 ]
