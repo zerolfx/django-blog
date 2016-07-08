38a82a5f295552bd2b7010a234b18bdc39566c3e
Zerol <zerolfx0@gmail.com>
2016-07-08 14:14:04 +0800

    fix a small bug

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'Aria/shanghai'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
